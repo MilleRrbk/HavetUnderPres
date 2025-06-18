a80df81b7b90a73a45eaf29aa0c5002f95b19407
Emma-Chantal Engledall <143702440+emmaengledall@users.noreply.github.com>
2025-06-18 17:47:00 +0200

    collider til båden virker

--- Assets/Scripts/MotorBoatMover.cs
+++ Assets/Scripts/MotorBoatMover.cs
@@ -5,7 +5,7 @@ public class MotorBoatMover : MonoBehaviour
     public Transform boat; // Træk din MotorBoat prefab herind i Inspector
     public Vector3 startPosition = new Vector3(49f, 17.98f, -25.6f);
     public Vector3 endPosition = new Vector3(-142f, 17.98f, -25.6f);
-    public float moveDuration = 15f; // Hvor lang tid det tager at sejle
+    public float moveDuration = 10f; // Hvor lang tid det tager at sejle
     private bool isMoving = false;
     private float moveTimer = 0f;
 
